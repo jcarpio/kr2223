c21f8a15605e6f2a921aa56c6a64d79a9aa888da
jcarpio <jose.carpio@gmail.com>
2023-03-06 12:53:40 +0100

    Update sudoku.pl

--- sudoku.pl
+++ sudoku.pl
@@ -1,15 +1,16 @@
-
-:- use_module(library(clpfd)).
-
-/*
-
-[[1, 2, 3],
- [2, 3, 1],
- [3, 1, 2]].
-
-sudu([1,2,3], [2,3,1], [3,1,2]).
+      [2,1,4,3,6,5,8,9,7].
+      [3,6,5,8,9,7,2,1,4].
+      [8,9,7,2,1,4,3,6,5].
+      [5,3,1,6,4,2,9,7,8].
+      [6,4,2,9,7,8,5,3,1].
+      [9,7,8,5,3,1,6,4,2].
 
 */
 
-soduku(Rows) :- length(Rows,3),
+soduku(Rows) :- length(Rows, 9),
+                maplist(same_length(Rows), Rows),
+				append(Rows, Vars), Vars ins 1..9,
+				maplist(all_distinct, Rows),
+				transpose(Rows, Columns),
+				maplist(all_distinct, Columns).
                 
