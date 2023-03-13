dff77763506684bdcd7d4f0cd923c77dca4df9a4
jcarpio <jose.carpio@gmail.com>
2023-03-13 13:33:40 +0100

    Update irregular_sudoku.pl

--- irregular_sudoku.pl
+++ irregular_sudoku.pl
@@ -25,7 +25,7 @@
 sudoku(Rows) :- length(Rows, 9),
     maplist(same_length(Rows), Rows),
 	append(Rows, Rs), Rs ins 1..9,
-	maplist(all_dictinct, Rows),
+	maplist(all_distinct, Rows),
 	transpose(Rows, Columns), 
 	maplist(all_distinct, Columns),
     Rows=[[E11,E12,E13,E14,E15,E16,E17,E18,E19],
@@ -38,22 +38,22 @@ sudoku(Rows) :- length(Rows, 9),
 		  [E81,E82,E83,E84,E85,E86,E87,E88,E89],
 		  [E91,E92,E93,E94,E95,E96,E97,E98,E99]
 		 ],
-		 all_dictinct([E11,E12,E13,E14,E15,E21,E22,E23,E24]),
-		 all_dictinct([E16,E17,E18,E19,E25,E26,E27,E28,E29]),
-		 all_dictinct([E31,E32,E41,E51,E52,E61,E62,E63,E71]),
-		 all_dictinct([E42,E43,E53,E54,E64,E72,E73,E74,E75]),
-		 all_dictinct([E33,E34,E44,E45,E55,E65,E66,E76,E77]),
-		 all_dictinct([E35,E36,E37,E38,E46,E56,E57,E67,E68]),
-		 all_dictinct([E39,E47,E48,E49,E58,E59,E69,E78,E79]),		 
-		 all_dictinct([E81,E82,E83,E84,E85,E91,E92,E93,E94]),
-		 all_dictinct([E86,E87,E88,E89,E95,E96,E97,E98,E99]).
+		 all_distinct([E11,E12,E13,E14,E15,E21,E22,E23,E24]),
+		 all_distinct([E16,E17,E18,E19,E25,E26,E27,E28,E29]),
+		 all_distinct([E31,E32,E41,E51,E52,E61,E62,E63,E71]),
+		 all_distinct([E42,E43,E53,E54,E64,E72,E73,E74,E75]),
+		 all_distinct([E33,E34,E44,E45,E55,E65,E66,E76,E77]),
+		 all_distinct([E35,E36,E37,E38,E46,E56,E57,E67,E68]),
+		 all_distinct([E39,E47,E48,E49,E58,E59,E69,E78,E79]),		 
+		 all_distinct([E81,E82,E83,E84,E85,E91,E92,E93,E94]),
+		 all_distinct([E86,E87,E88,E89,E95,E96,E97,E98,E99]).
 		 
 		 
 irregular_sudoku1([
                     [_,_,_,_,1,8,9,_,2],
 					[_,_,6,2,_,_,1,_,_],
 					[_,_,_,6,_,_,7,_,_],
-					[4,6,_,_,_,_,_,_,2],
+					[4,6,_,_,_,_,_,8,5],
 					[_,_,_,_,3,_,_,_,_],
 					[6,7,_,_,_,_,_,1,3],
 					[_,_,5,_,_,7,_,_,_],
@@ -62,4 +62,4 @@ irregular_sudoku1([
 				  ]).
 				  
 				  
-% irregular_sudoku1(S), sudoku(S), maplist(labeling([ff]), S).
+% irregular_sudoku1(S), sudoku(S), maplist(labeling([ff]), S), write(S).
